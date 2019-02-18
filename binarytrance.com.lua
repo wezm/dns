8bb4c474f2ade6b3ce9beeeba1b2eccd8633eca0
Wesley Moore <wes@wezm.net>
2019-02-19 07:51:56 +1100

    Add binarytrance.com

---
+++ binarytrance.com.lua
@@ -0,0 +1,26 @@
+-- Zone: bitcannon.net
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "binarytrance.com"
+
+-- A records
+a(_a, "45.55.46.163")
+a("eforce.binarytrance.com", "45.55.46.163")
+a("ferry.binarytrance.com", "199.180.255.220")
+a("gareth.binarytrance.com", "174.138.53.121")
+a("headhunterz.binarytrance.com", "45.32.240.91")
+
+-- CNAME records
+cname("www", _a)
+cname("sriqdqs21x8lo9xl8ybz0900zjhqhbui", "s20160306142507.binarytrance.com")
+cname("3087948cc85d5610e4a80cbc084eac43", "verify.bing.com")
+
+-- TXT
+txt(_a, "google-site-verification=ayfIGKxUXBaLAnrW1UAAhEwnnPzphCx9DrpmAreGR_A")
+
+-- CAA records
+-- caa("", "letsencrypt.org", "issue")
+-- caa("", "mailto:joe@bitcannon.net", "iodef")
+
+-- Templates (see templates/*)
+fastmail(_a)
