ec0df2d304f0fe605c3e70088813c6b8cbf9bcd0
Wesley Moore <wes@wezm.net>
2019-02-19 08:09:09 +1100

    Add wezm.net

---
+++ wezm.net.lua
@@ -0,0 +1,32 @@
+-- Zone: bitcannon.net
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "wezm.net"
+
+-- A records
+a(_a, "45.55.46.163")
+
+-- CNAME records
+cname("bigd", "eforce.binarytrance.com")
+cname("doc", "eforce.binarytrance.com")
+cname("f6a360bece1693ece78823de691c3f96", "verify.bing.com")
+cname("files", "eforce.binarytrance.com")
+cname("git", "eforce.binarytrance.com")
+cname("html", "eforce.binarytrance.com")
+cname("kyoto-client", "eforce.binarytrance.com")
+cname("mattermost", "eforce.binarytrance.com")
+cname("menus", "eforce.binarytrance.com")
+cname("node-genx", "eforce.binarytrance.com")
+cname("pkg", "web.messagingengine.com")
+cname("releases", "web.messagingengine.com")
+cname("www", "eforce.binarytrance.com")
+
+-- TXT
+txt(_a, "google-site-verification:t1zIVbO1K7Xuo734w0tEdq6JCjvjMjrXq2FG3L2lkdg")
+
+-- CAA records
+-- caa("", "letsencrypt.org", "issue")
+-- caa("", "mailto:joe@bitcannon.net", "iodef")
+
+-- Templates (see templates/*)
+fastmail(_a)
