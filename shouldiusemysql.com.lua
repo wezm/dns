5817f659d2f734152dcdf975735d641252abddb1
Wesley Moore <wes@wezm.net>
2019-02-24 11:17:09 +1100

    Add shouldiusemysql.com

---
+++ shouldiusemysql.com.lua
@@ -0,0 +1,20 @@
+-- Zone: bitcannon.net
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "bitcannon.net"
+
+-- A records
+a(_a, "45.55.46.163")
+
+-- CNAME records
+cname("www", _a)
+
+-- TXT
+-- txt(_a, "google-site-verification=GQixNkjawUBZMHnviTCL2IsJXrtOQYcMbAHj6FD55_Q")
+
+-- CAA records
+-- caa("", "letsencrypt.org", "issue")
+-- caa("", "mailto:joe@bitcannon.net", "iodef")
+
+-- Templates (see templates/*)
+-- fastmail(_a)
