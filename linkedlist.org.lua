47e841c7c9afc7b5ecaec8707b85a0dde8b72668
Wesley Moore <wes@wezm.net>
2019-02-25 11:24:24 +1100

    Add linkedlist.org

---
+++ linkedlist.org.lua
@@ -0,0 +1,20 @@
+-- Zone: linkedlist.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "linkedlist.org"
+
+-- A records
+a(_a, "45.55.46.163")
+
+-- CNAME records
+cname("www", _a)
+
+-- TXT
+txt(_a, "google-site-verification=3HHylLz5MsixZgJZsf5NTHVXg93FHs1XQ2ElbcropQY")
+
+-- CAA records
+-- caa("", "letsencrypt.org", "issue")
+-- caa("", "mailto:joe@linkedlist.org", "iodef")
+
+-- Templates (see templates/*)
+-- fastmail(_a)
