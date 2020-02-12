4090560258aad0478837e82ca7c4a35eca62da79
Wesley Moore <wes@wezm.net>
2020-02-12 17:36:02 +1100

    Add bin.rs

---
+++ bin.rs.lua
@@ -0,0 +1,20 @@
+-- Zone: bin.rs
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "bin.rs"
+
+-- A records
+a(_a, "45.76.112.252")
+
+-- CNAME records
+cname("www", _a)
+
+-- TXT
+-- txt(_a, "google-site-verification=GQixNkjawUBZMHnviTCL2IsJXrtOQYcMbAHj6FD55_Q")
+
+-- CAA records
+caa("", "letsencrypt.org", "issue")
+caa("", "mailto:wes@binarytrance.com", "iodef")
+
+-- Templates (see templates/*)
+-- fastmail(_a)
