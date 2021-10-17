ef9bbe10e4360aa013e3ee89db9728cce03a1b8b
Wesley Moore <wes@wezm.net>
2021-10-17 16:21:22 +1000

    Add 7bit.org

---
+++ 7bit.org.lua
@@ -0,0 +1,22 @@
+-- Zone: 7bit.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "7bit.org"
+
+-- A records
+a(_a, "45.76.112.252")
+
+-- CNAME records
+cname("www", _a)
+cname("dewpoint", _a)
+
+-- TXT
+
+-- CAA records
+caa("", "letsencrypt.org", "issue")
+caa("", "mailto:wes@binarytrance.com", "iodef")
+
+-- MX
+
+-- Templates (see templates/*)
+-- fastmail(_a)
