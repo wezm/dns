4fdae41c6fc367233be0516f75827fa30ca6a241
Wesley Moore <wes@wezm.net>
2023-01-25 09:17:12 +1000

    Add mastodon.decentralised.social

--- decentralised.social.lua
+++ decentralised.social.lua
@@ -9,6 +9,7 @@ a(_a, "45.76.112.252")
 -- CNAME records
 cname("www", _a)
 cname("nitter", _a)
+cname("mastodon", "vip.masto.host", 3600)
 
 -- TXT
 -- txt(_a, "google-site-verification=GQixNkjawUBZMHnviTCL2IsJXrtOQYcMbAHj6FD55_Q")
