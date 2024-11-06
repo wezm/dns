3af342d356ae51e10976188889c7d6064582359c
Wesley Moore <wes@wezm.net>
2024-11-07 08:33:06 +1000

    Add linkedlist POPs

--- linkedlist.org.lua
+++ linkedlist.org.lua
@@ -8,6 +8,8 @@ a(_a, "45.76.112.252")
 
 -- CNAME records
 cname("www", _a)
+cname("ny", "192.3.44.156")
+cname("fr", "193.42.61.5")
 
 -- TXT
 txt(_a, "google-site-verification=3HHylLz5MsixZgJZsf5NTHVXg93FHs1XQ2ElbcropQY")
