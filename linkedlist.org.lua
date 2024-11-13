42042630009bc61b65b182e84728f91bfbb2a8e3
Wesley Moore <wes@wezm.net>
2024-11-14 07:54:08 +1000

    linkedlist: _atproto take 2

--- linkedlist.org.lua
+++ linkedlist.org.lua
@@ -14,7 +14,7 @@ cname("www", _a)
 
 -- TXT
 txt(_a, "google-site-verification=3HHylLz5MsixZgJZsf5NTHVXg93FHs1XQ2ElbcropQY")
-txt("_atproto", "did:plc:clkmqjwupe5zxeppfpl2i2ci")
+txt("_atproto", "did=did:plc:clkmqjwupe5zxeppfpl2i2ci")
 
 -- CAA records
 caa("", "letsencrypt.org", "issue")
