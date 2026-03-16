abfa0a16cab2392c2f7f69b7ca4205e500529934
Wesley Moore <wes@wezm.net>
2026-03-16 15:42:57 +1000

    Add casuarina.org

---
+++ casuarina.org.lua
@@ -0,0 +1,23 @@
+-- Zone: casuarina.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "casuarina.org"
+
+-- A records
+a(_a, "185.187.152.7")
+aaaa(_a, "2a05:b0c4:1::3")
+
+-- CNAME records
+-- cname("www", _a)
+
+-- TXT
+-- txt(_a, "google-site-verification=3HHylLz5MsixZgJZsf5NTHVXg93FHs1XQ2ElbcropQY")
+txt("_git-pages-repository", "https://codeberg.org/casuarina/casuarina.org.git")
+
+-- CAA records
+-- caa("", "letsencrypt.org", "issue")
+-- caa("", "mailto:wes@binarytrance.com", "iodef")
+
+-- Templates (see templates/*)
+-- fastmail(_a)
+
